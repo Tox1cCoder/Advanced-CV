2f2711df13686fcc7e6ed17885ff96b2fcbaa3b8
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-03-28 23:12:43 +0700

    Add files via upload

---
+++ PoseTrackingMin.py
@@ -0,0 +1,31 @@
+import time
+
+import cv2 as cv
+import mediapipe as mp
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv.VideoCapture("./Videos/vid2.mp4")
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv.circle(img, (cx,cy), 5, (255,0,0), cv.FILLED)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv.putText(img, str(int(fps)), (70, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv.imshow("Image", img)
+
+    cv.waitKey(1)
