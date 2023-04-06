7b4bd2d78106e59783f0af152fbf3c1b0a7a6088
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-04-06 22:07:09 +0700

    Update PoseTrackingMin.py

--- poseEstimation/PoseTrackingMin.py
+++ poseEstimation/PoseTrackingMin.py
@@ -1,84 +1,32 @@
-import math
 import time
 
 import cv2
 import mediapipe as mp
 
+mpPose = mp.solutions.pose
+mpDraw = mp.solutions.drawing_utils
+pose = mpPose.Pose()
 
-class poseDetector():
-    def __init__(self, mode=False, complexity=1, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
-        self.mode = mode
-        self.complexity = complexity
-        self.upBody = upBody
-        self.smooth = smooth
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
-        self.mpDraw = mp.solutions.drawing_utils
-        self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, self.complexity, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+video_capture = cv2.VideoCapture(0)
+prev_time = 0
 
-    def findPose(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.pose.process(imgRGB)
-        if self.results.pose_landmarks:
-            if draw:
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
-                                           self.mpPose.POSE_CONNECTIONS)
-        return img
+while True:
+    _, img = video_capture.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
 
-    def findPosition(self, img, draw=True):
-        self.lmList = []
-        if self.results.pose_landmarks:
-            for id, lm in enumerate(self.results.pose_landmarks.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                self.lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-        return self.lmList
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, landmark in enumerate(results.pose_landmarks.landmark):
+            height, width, _ = img.shape
+            x, y = int(landmark.x * width), int(landmark.y * height)
+            if id == 0:
+                cv2.circle(img, (x, y), 10, (255, 0, 0), cv2.FILLED)
 
-    def findAngle(self, img, p1, p2, p3, draw=True):
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        x3, y3 = self.lmList[p3][1:]
-        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
-        
-        if angle == 0:
-            angle += 360
-        
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
-            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
-            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
-            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        return angle
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
 
-
-def main():
-    cap = cv2.VideoCapture("Videos/vid2.mp4")
-    pTime = 0
-    detector = poseDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img, draw=False)
-        if len(lmList) != 0:
-            print(lmList[14])
-            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 0), 3)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
+    cv2.putText(img, str(f'FPS:{int(fps)}'), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
